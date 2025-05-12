04068e74a27a1def4cddc1dc7199de40b98bd8cc
dweight fuentes <dwieghtpro@gmail.com>
2025-05-12 13:55:56 +0800

    build fix updates

--- src/server/index.ts
+++
@@ -1,7 +0,0 @@
-import { youtubeLink } from "./routers/youtubeLinkRouter";
-import { notepadRouter } from "./routers/notepadRouter";
-import { mergeRouters } from "./trpc";
-
-export const appRouter = mergeRouters(youtubeLink, notepadRouter);
-
-export type AppRouter = typeof appRouter;
