4474012e70ebd5162f4e0b11fc6280cff2fc3e37
dweight fuentes <dwieghtpro@gmail.com>
2025-05-12 13:38:43 +0800

    resolved mc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,4 @@
 // app/layout.tsx
-<<<<<<< HEAD
-import { Providers } from "./_trpc/Provider";
-import type { Metadata } from "next";
-import "./globals.css";
-export const metadata: Metadata = {
-    title: "MiPocket",
-    description: "Your personal pocket app",
-};
-
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return (
-        <html lang="en">
-            <body>
-                <Providers>{children}</Providers>
-            </body>
-        </html>
-    );
-=======
 import type { Metadata } from "next";
 import "./globals.css";
 export const metadata: Metadata = {
@@ -38,5 +16,4 @@ export default function RootLayout({
       <body className="p-2">{children}</body>
     </html>
   );
->>>>>>> d42dac0 (initial portfolio)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,5 @@
 "use client";
 
-<<<<<<< HEAD
-import Link from "next/link";
-
-function HomePage() {
-    return (
-        <div className="border h-screen flex items-center justify-center">
-            <div className="p-6 bg-white shadow-lg rounded-lg w-full max-w-sm">
-                <Link
-                    href="/login"
-                    className="block text-center w-full px-4 py-2 text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
-                >
-                    Signin
-                </Link>
-            </div>
-        </div>
-    );
-}
-
-export default HomePage;
-=======
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
@@ -781,4 +761,3 @@ export default function HomePage() {
     </div>
   );
 }
->>>>>>> d42dac0 (initial portfolio)
